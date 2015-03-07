490a922391a0f00a04ed7655bad809ad850fb5bc
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2015-03-07 16:36:41 +0100

    Initialization of the LogParser class.     * Creation of the public interface.     * Initialization of internal attributes.     * TODO : all the intelligence.

---
+++ ddrla/logParser.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2015 Kevin Hagner
+#
+# ddrla is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ddrla is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ddrla.  If not, see <http://www.gnu.org/licenses/>.
+
+class LogParser:
+    """
+        Parse output logs from ddrescue command for being processed by
+        the software.
+    """
+    logsDictionary = []
+    logsStatistics = {}
+
+    def __init__(self, file):
+        self.__initLogsStatistics();
+        self.__processLogParsing(file)
+
+    def getLogsDictionnary(self):
+        return self.logsDictionary;
+
+    def getLogsStatistics(self):
+        return self.logsStatistics;
+
+    def __initLogsStatistics(self):
+        self.logsStatistics['nontried'] = 0
+        self.logsStatistics['rescued'] = 0
+        self.logsStatistics['nontrimmed'] = 0
+        self.logsStatistics['nonsplit'] = 0
+        self.logsStatistics['bad'] = 0
+        self.logsStatistics['total'] = 0
+
+    def __processLogParsing(self, file):
+        #TODO
+
