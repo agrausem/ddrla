e50cb98d82aca3ddae03cfd08cee1d7d37d62b23
Kevin Hagner <jsaipakoimetr@spyzone.fr>
2015-03-09 19:55:52 +0100

    Initialization of the unit test for the logParser class.     * TODO : set a temporary testing file.     * TODO : close and remove this file.     * TODO : test the public method getLogsDictionary.     * TODO : test the public method getLogsStatistics.

---
+++ tests/testLogParser.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2015 Kevin Hagner
+#
+# ddrla is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ddrla is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ddrla.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+class TestLogParser(unittest.TestCase):
+
+    def setUp(self):
+        #TODO
+
+    def tearDown(self):
+        #TODO
+
+    def testGetLogsDictionnary(self):
+        #TODO
+
+    def testGetLogsStatistics(self):
+        #TODO
+
+if __name__ == '__main__':
+    unittest.main()
